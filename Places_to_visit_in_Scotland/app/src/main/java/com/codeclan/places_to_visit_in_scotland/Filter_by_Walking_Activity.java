8800f746bacd2443ac2dc28b1376b785707a6371
Rob <robflettcc@gmail.com>
2017-03-22 11:56:24 +0000

    added filter button

--- Places_to_visit_in_Scotland/app/src/main/java/com/codeclan/places_to_visit_in_scotland/Filter_by_Walking_Activity.java
+++ Places_to_visit_in_Scotland/app/src/main/java/com/codeclan/places_to_visit_in_scotland/Filter_by_Walking_Activity.java
@@ -2,6 +2,8 @@ package com.codeclan.places_to_visit_in_scotland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Filter_by_Walking_Activity extends AppCompatActivity {
 
@@ -12,5 +14,16 @@ public class Filter_by_Walking_Activity extends AppCompatActivity {
 
         TopPlacesinScotlandtoVisit walkingPlaces = new TopPlacesinScotlandtoVisit();
         walkingPlaces.getByActivity("Walking");
+//        Log.d("filtered place", walkingPlaces.getByActivity("Walking"));
+
+        String filteredPlaces = "";
+
+        for(Place place : walkingPlaces.getByActivity("Walking")){
+            filteredPlaces += place.getPlacename() + " in " + place.getLocation() + " is great for walking\n";
+        }
+
+        TextView tv = (TextView) findViewById(R.id.filtered_places);
+        tv.setText(filteredPlaces);
+
     }
 }
